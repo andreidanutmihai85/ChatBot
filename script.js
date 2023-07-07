d3c0353418a46c18c30ff1621ee88c051fad48c3
Andrei Danut Mihai <138859815+andreidanutmihai85@users.noreply.github.com>
2023-07-07 11:27:56 +0300

    Chat Bot
    Chat bot with preset questions

---
+++ script.js
@@ -0,0 +1,55 @@
+let buton1 = document.getElementById("buton1");
+let buton2 = document.getElementById("buton2");
+
+let intrebare1 = document.getElementById("intrebare1");
+let intrebare2 = document.getElementById("intrebare2");
+let intrebare3 = document.getElementById("intrebare3");
+
+let messageRight = document.getElementById("mesaj1");
+let messageRight2 = document.getElementById("mesaj2");
+let messageRight4 = document.getElementById("mesaj4");
+let dots = document.getElementById("dots");
+let dots2 = document.getElementById("dots2");
+
+function Functie1(){
+    // return buton1.style.color = "blue";
+
+//  const ras1 =  messageRight.style.display = "block";
+    const ras2 =  intrebare1.style.display = "block";
+
+    return ras2;
+}
+
+
+function Functie2(){
+    // return buton1.style.color = "blue";
+   
+    const ras1 = messageRight.style.display = "block";
+    const ras2 = dots.style.display = "none";
+    return ras1, ras2;
+ 
+};
+
+// function ascunde(){
+
+//    const dots1 =  dots.style.display = 'none';
+//    return dots1;
+// }
+
+function Functie3(){
+    // return buton1.style.color = "blue";
+
+//  const ras1 =  messageRight.style.display = "block";
+    const ras2 =  intrebare3.style.display = "block";
+
+    return ras2;
+}
+
+function Functie4(){
+    // return buton1.style.color = "blue";
+   
+    const ras1 = messageRight4.style.display = "block";
+    const ras2 = dots2.style.display = "none";
+    return ras1, ras2;
+ 
+};
